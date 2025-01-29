c747e741ccb7aead1979649555ce2ffeaaf17694
BenjaminDestefanis <benjaminisraelb@gmail.com>
2025-01-29 14:09:34 -0300

    Landing-Page

--- frontend--/src/App.tsx
+++ frontend--/src/App.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import Greeting from "./components/Greeting";
+import LandingPage from "./pages/Landing";
 import  "./App.css"
 
 function App(): JSX.Element {
   return (
     <div className="App">
-      <h1>¡Bienvenido a tu plataforma de aprendizaje!</h1>
-      <p>Comencemos a construir.</p>
+      <h1>Aprende coneptos rersolviendo problemas cotidianos utilizando </h1>
+      <p>Domina matemáticas, programación, estadísticas y más con ejercicios prácticos y visuales.</p>
 
       <Greeting name="Benjamin" />
+
+
+
+
+      {/* LANDING */}
+
+    <LandingPage />
     </div>
   );
 }
