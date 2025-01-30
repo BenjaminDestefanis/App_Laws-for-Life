aba8360380e5592cae0ddf3c65d304f0a8a33481
BenjaminDestefanis <benjaminisraelb@gmail.com>
2025-01-30 16:14:59 -0300

    NavBar Funcionando

--- frontend--/src/App.tsx
+++ frontend--/src/App.tsx
@@ -2,17 +2,30 @@ import React from "react";
 import Greeting from "./components/Greeting";
 import LandingPage from "./pages/Landing";
 import  TypingEffect from "./components/Header";
+import { Routes, Route, useRoutes } from "react-router-dom";;
+import TrainingSeccion from "./pages/TrainingSeccion";
+import Navbar from "./components/NavBar";
 import  "./App.css"
+
+const AppRoutes = () => {
+  return useRoutes([
+    { path: "/training", element: <TrainingSeccion />}
+  ])
+}
 
 function App(): JSX.Element {
   return (
     <div className="App">
+      <Navbar />
+
       <h1>Aprende coneptos rersolviendo problemas cotidianos utilizando </h1>
       <p>Domina matemáticas, programación, estadísticas y más con ejercicios prácticos y visuales.</p>
 
       {/* <Greeting name="Benjamin" /> */}
-
-
+    
+    {/* <Routes>
+      <Route path="/training" element={<TrainingSeccion />} />
+    </Routes> */}
 
 
       {/* LANDING */}
---
+++ frontend--/src/components/NavBar.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { AppBar, Toolbar, Button, Container } from '@mui/material';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+/* import App from '../App'; */
+import TrainingSeccion from '../pages/TrainingSeccion';
+
+
+// Tipado de rutas
+type RouteConfig = {
+  path: string;
+  element: React.ReactNode;
+};
+
+// Configuración de rutas
+const routes: RouteConfig[] = [
+  /* { path: '/', element: <App /> }, */
+  { path: '/training', element: <TrainingSeccion /> }, // Nueva ruta
+];
+
+const Navbar: React.FC = () => {
+  return (
+    <Router>
+      <AppBar position="static">
+        <Toolbar>
+          {routes.map((route) => (
+            <Button key={route.path} color="inherit" component={Link} to={route.path}>
+              {route.path === '/' ? 'Home' : route.path.slice(1)}
+            </Button>
+          ))}
+        </Toolbar>
+      </AppBar>
+
+      <Container>
+        <Routes>
+          {routes.map((route) => (
+            <Route key={route.path} path={route.path} element={route.element} />
+          ))}
+        </Routes>
+      </Container>
+    </Router>
+  );
+};
+
+export default Navbar;
